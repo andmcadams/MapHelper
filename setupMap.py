d08e7f5ece229fd71acf599cb4739d8318bcaaa1
Andrew McAdams <andmcadams@gmail.com>
2020-03-19 18:32:34 -0500

    Create layer groups for other suggested layer folders

--- setupMap.py
+++ setupMap.py
@@ -59,17 +59,23 @@ def python_make_map(width, height, squareSize, background):
 	l.visible = False
 	img.add_layer(l, 0)
 
+	lg = gimp.GroupLayer(img, "Objects Group")
+	pdb.gimp_image_insert_layer(img, lg, None, 0)
+
 	l = pdb.gimp_layer_new(img, width, height, 1, "Objects", OPAQUE, 0)
 	l.visible = False
-	img.add_layer(l, 0)
+	img.insert_layer(l, lg, 0)
 
 	l = pdb.gimp_layer_new(img, width, height, 1, "Icons", OPAQUE, 0)
 	l.visible = False
 	img.add_layer(l, 0)
 
+	lg = gimp.GroupLayer(img, "Text Group")
+	pdb.gimp_image_insert_layer(img, lg, None, 0)
+
 	l = pdb.gimp_layer_new(img, width, height, 1, "Text", OPAQUE, 0)
 	l.visible = False
-	img.add_layer(l, 0)
+	img.insert_layer(l, lg, 0)
 
 	# pdb.plug_in_grid doesn't actually seem to create gridlines sadly. Maybe I'm just calling it wrong?
 	# pdb.plug_in_grid(img, img.layers[-1], 0, 10, 0, (0x00, 0x00, 0x00, 0xFF), 255, 0, 10, 0, (0xFF, 0x00, 0x00, 0xFF), 255, 0, 0, 0, (0xFF, 0x00, 0x00, 0xFF), 255)
